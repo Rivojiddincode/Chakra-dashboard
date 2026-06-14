084dcdde31c873b72960ce0b9b1431e6f656e343
RIJOL <rijolcoderuz@gmail.com>
2026-06-14 17:24:37 +0500

    chore: completely removed font files and styles

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import vue from '@vitejs/plugin-vue'
+import path from 'path' // Buni qo'shing
 export default defineConfig({
-  plugins: [react()],
+  
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // @ belgisini src papkasiga bog'lash
+    },
+
+
+  },
 })
