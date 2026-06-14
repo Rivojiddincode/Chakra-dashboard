5288fd6276da10e65c3bb023e8b9cd0b609b410a
RIJOL <rijolcoderuz@gmail.com>
2026-06-14 17:36:06 +0500

    fix: changed vite config from vue to react

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path' // Buni qo'shing
+import react from '@vitejs/plugin-react' // Bu yer React uchun to'g'rilandi
+
+// https://vite.dev
 export default defineConfig({
-  
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // @ belgisini src papkasiga bog'lash
-    },
-
-
-  },
+  plugins: [react()],
 })
+
